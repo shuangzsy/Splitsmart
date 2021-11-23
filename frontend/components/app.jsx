8312dac48776fea7beabd38906fc32d5da5216bc
shuang zheng <shuangzheng16@gmail.com>
2021-11-23 10:11:34 -0800

    styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import UserProfileContainer from './user/user_profile_container';
+import mainPage from './main_page';
 import {
   Route,
   Redirect,
@@ -13,7 +14,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <>
-      <div className='header'>
+      {/* <div className='header'>
         <div>
         <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1637653233/IMG_5588_iksqs5.jpg"/>
         </div>
@@ -22,9 +23,15 @@ const App = () => (
           <Link className='login' to='/login'>Log In</Link>
           <Link className='signup' to='/signup'>Sign Up</Link>
         </ul>
-    </div>
-      <AuthRoute path="/login" component={LoginFormContainer} />
-      <AuthRoute path="/signup" component={SignupFormContainer} />
+    </div> */}
+    <Switch>
+      <AuthRoute exact path="/" component={mainPage} /> 
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
+    {/* <div>
+      <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1637687229/Screen_Shot_2021-11-23_at_9.05.40_AM_umuqrr.png" alt="" />
+    </div> */}
       {/* <ProtectedRoute path='/dashboard' component={DashboardNavbar}/> */}
   </>
 
---
+++ frontend/components/main_page.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const mainPage = () => {
+  return (
+    <div>
+    <div className='header'>
+      <div>
+        <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1637653233/IMG_5588_iksqs5.jpg" />
+      </div>
+
+      <ul>
+        <Link className='login' to='/login'>Log In</Link>
+        <Link className='signup' to='/signup'>Sign Up</Link>
+      </ul>
+    </div>
+      <div>
+          <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1637687229/Screen_Shot_2021-11-23_at_9.05.40_AM_umuqrr.png" alt="" />
+      </div>
+    </div>
+
+  )
+}
+
+export default mainPage;
