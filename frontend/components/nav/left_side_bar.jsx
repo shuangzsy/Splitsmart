6546ad82129f1a2067565ea3807ec86233136492
shuang zheng <shuangzheng16@gmail.com>
2021-12-03 02:55:53 -0800

    version 1

--- frontend/components/nav/left_side_bar.jsx
+++ frontend/components/nav/left_side_bar.jsx
@@ -11,9 +11,15 @@ class LeftNav extends React.Component {
       <div>
         <ul className="left_side_bar_list">
           <li className="dashboard"><img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638136934/Screen_Shot_2021-11-28_at_1.24.41_PM_1_kqmi8h.png"/><Link to="/dashboard">Dashboard</Link></li>
-          <li className="activity"><Link to="/activity">Recent activity</Link></li>
-          <li className="filter"><input type="text" placeholder="filter by name"/></li>
-          <li className="all"><Link to="/all">All Expenses</Link></li>
+          <li className="activity">
+            <Link to="/activity">Recent activity</Link>
+          </li>
+          <li className="filter">
+            <input type="text" placeholder="filter by name"/>
+          </li>
+          <li className="all">
+            <Link to="/all">All Expenses</Link>
+          </li>
           <li><span>Groups</span></li>
           <li><span>Friends</span></li>
         </ul>
