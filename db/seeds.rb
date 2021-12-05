50c0f076b6e90718ea3031b302561093dbae06b5
shuang zheng <shuangzheng16@gmail.com>
2021-12-04 18:03:52 -0800

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 demoUser = User.create(email:"sz3@sz.com", username:"sz3", password:"1234567")
-expense_1 = Expense.create(description:"lunch", total_amount: 50.2, creator_id: 1, settled: false )
-expense_2 = Expense.create(description:"jenny's kitchen", total_amount: 25.52, creator_id: 8, settled: false )
 ####
