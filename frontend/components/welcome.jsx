a269aa7f0123d2b083260f8b21321fd7e128ae5e
shuang zheng <shuangzheng16@gmail.com>
2021-12-23 16:25:46 -0800

    welcome page sign up button

--- frontend/components/welcome.jsx
+++ frontend/components/welcome.jsx
@@ -7,7 +7,9 @@ const WelcomePage = () => {
     <div>
       <MainNav/>
       <div>
-        <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638496432/Screen_Shot_2021-12-02_at_4.11.23_PM_dl3w1z.png" alt="" />
+        <Link to='/signup'>
+          <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638496432/Screen_Shot_2021-12-02_at_4.11.23_PM_dl3w1z.png" alt="" />
+        </Link>
         <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638492474/Screen_Shot_2021-12-02_at_4.11.34_PM_ntncoq.png" alt="" />
         <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638492502/Screen_Shot_2021-12-02_at_4.11.45_PM_b6lpxp.png" alt="" />
         <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638492474/Screen_Shot_2021-12-02_at_4.11.34_PM_ntncoq.png" alt="" />
