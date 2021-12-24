622ff267a76c2948efdc0d35e8ea042cb181b840
shuang zheng <shuangzheng16@gmail.com>
2021-12-24 11:09:46 -0800

    edit expense container

--- frontend/components/expense/edit_expense_form_container.jsx
+++ frontend/components/expense/edit_expense_form_container.jsx
@@ -3,6 +3,25 @@ import { updateExpense } from '../../actions/expense_action';
 import ExpenseForm from './expense_form';
 import { withRouter } from 'react-router-dom';
 
+class EditExpenseForm extends React.Component {
+  componentDidMount() {
+    this.props.requestExpense(this.props.match.params.expenseId)
+  }
+
+  render() {
+    const { expense, splits, formType, submitExpense } = this.props;
+
+    if (!expense) return null;
+    return (
+      <ExpenseForm
+        expense={expense}
+        splits = {splits}
+        formType={formType}
+        submitExpense={submitExpense} />
+    );
+  }
+}
+
 const mSTP = (state, ownProps) => ({
   expense: state.entities.expenses[ownProps.match.params.expenseId],
   splits: [[state.session.currentUser.email, -0.5], ['', 0.5]],
