0d764fe18fb30fe49f18d7b1695a151508d97f8d
shuang zheng <shuangzheng16@gmail.com>
2021-12-26 13:39:22 -0800

    display block for expenses details

--- frontend/components/expense/expense_index_item.jsx
+++ frontend/components/expense/expense_index_item.jsx
@@ -2,23 +2,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ExpenseShowContainer from './expense_show_container';
 
+
+
 const ExpenseIndexItem = ({ expense, deleteExpense }) => {
+  function clickToShow() {
+    const expenseItem = document.getElementById(`${expense.id}`);
+    console.log(expenseItem.style.display)
+    if (expenseItem.style.display === "none" || expenseItem.style.display === ""){
+      expenseItem.style.display = "block"
+    }else {
+      expenseItem.style.display = "none"
+    }
+  }
+
   return (
-      <div className="expense-item-details">
-        <div className="expense-info">
-          <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638476337/Screen_Shot_2021-12-02_at_12.17.59_PM_mk8tvd.png" alt="" />
-          <div>
-            <div className="expense-description">{expense.description}</div>
-            <div className="total-amount">${expense.totalAmount}</div>
-            <div className="edit-by">Added by ... on {expense.createdAt}</div>
-            <button className="edit-expense-button">Edit Expense</button >
+      <>
+        <div className="expense-overview" onClick={clickToShow}>
+          <span>{expense.createdAt}</span>
+          <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638476337/Screen_Shot_2021-12-02_at_12.17.59_PM_mk8tvd.png" alt="" /> 
+          <span>{expense.description}</span>
+        </div>
+        <div id={expense.id} className="all-expense-item-details">
+          <div className="expense-info">
+            <div>
+            <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1640543861/catg_qxrt5q.png" alt="" />
+            </div>
+            <div>
+              <div className="expense-description">{expense.description}</div>
+              <div className="total-amount">${expense.totalAmount}</div>
+              <div className="edit-by">Added by ... on {expense.createdAt}</div>
+              <button className="edit-expense-button">Edit Expense</button >
+            </div>
+          </div>
+          <hr />
+          <div><ExpenseShowContainer expense={expense} /></div>
+          <div>       
+            <button onClick={() => deleteExpense(expense.id)}>Delete</button>
           </div>
         </div>
-        <div><ExpenseShowContainer expense={expense} /></div>
-        <div>       
-          <button onClick={() => deleteExpense(expense.id)}>Delete</button>
-        </div>
-      </div>
+      </>
   )
 }
 
--- frontend/components/expense/expense_show.jsx
+++ frontend/components/expense/expense_show.jsx
@@ -12,14 +12,24 @@ class ExpenseShow extends React.Component {
     if (this.props.splits[0] === undefined) return "";
     return (
       <div>
-        <div>{
+        <div className="split-datails">{
           this.props.splits.map(split => {
-            return(
-              <div>
-                <li>{split.username}</li>
-                <li>{split.oweAmount}</li>
-              </div>
-            )
+            if (split.oweAmount > 0) {
+              return(
+                <div>
+                  <div>
+                    {/* <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638517195/Screen_Shot_2021-12-02_at_11.39.35_PM_bwjsnw.png" alt="" />  */}
+                    <strong>{split.username}</strong> owes <strong>${split.oweAmount}</strong></div>
+                </div>)
+                }
+            else {
+              return(
+                <div>
+                  <div>
+                    {/* <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638517195/Screen_Shot_2021-12-02_at_11.39.28_PM_rh8ezb.png" alt="" /> */}
+                    <strong>{split.username}</strong> paid <strong>${split.oweAmount * 2}</strong> and owes <strong>${-split.oweAmount}</strong></div>
+                </div>)
+            }
           })
         }</div>
       </div>
