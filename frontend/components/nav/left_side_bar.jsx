6e79845a656b4cc3a146223d2b9d887bf35b18b9
shuang zheng <shuangzheng16@gmail.com>
2021-12-28 22:08:50 -0800

    left side bar css

--- frontend/components/nav/left_side_bar.jsx
+++ frontend/components/nav/left_side_bar.jsx
@@ -26,7 +26,7 @@ class LeftNav extends React.Component {
     return (
       <div className='left-side-bar-list-container'>
         <ul className="left_side_bar_list">
-          <li className="dashboard"><img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638136934/Screen_Shot_2021-11-28_at_1.24.41_PM_1_kqmi8h.png"/><Link to="/dashboard">Dashboard</Link></li>
+          <li className="dashboard"><Link to="/dashboard">✉️ Dashboard</Link></li>
           <li className="activity">
             <Link className="activity-list" to="/activity">🏳️ Recent activity</Link>
           </li>
--- frontend/components/user/user_profile.jsx
+++ frontend/components/user/user_profile.jsx
@@ -14,14 +14,13 @@ class UserProfile extends React.Component {
   }
 
   render() {
- 
     return (
       <div>
         {/* <Modal/> */}
         <div><DashboardNav currentUser={this.props.currentUser} UserLogout={this.props.UserLogout} /></div>
         <div className="profile-body">
           <div className='left_side_bar'><LeftNav expenses = {this.props.expenses} splits={this.props.splits} currentUser={this.props.currentUser}/></div>
-          <div className='center_bar'><CenterNav AddExpense={this.props.AddExpense}/></div>
+          <div className='center_bar'><CenterNav AddExpense={this.props.AddExpense} splits={this.props.splits} currentUser={this.props.currentUser}/></div>
           <div className='right-bar'>
             <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638552017/Screen_Shot_2021-12-03_at_9.19.58_AM_dleon5.png" alt="" />
           </div>      
