0af2420a0c1598a48c3a1fdd407f10d2bb75b13e
shuang zheng <shuangzheng16@gmail.com>
2022-01-06 11:07:45 -0800

    add friend list css

--- frontend/components/friends/friend_index_item.jsx
+++ frontend/components/friends/friend_index_item.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 
 const FriendIndexItem = ({ friend, deleteFriend }) => {
   return (
-    <li>
-      <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638645241/profile-icon-png-898_3_ofszd1.png" alt="" />
-      <div style={{display:'inline-block'}}>{friend.friendEmail.substring(0, friend.friendEmail.lastIndexOf("@"))}</div>
-      <button onClick={() => deleteFriend(friend.id)}>X</button>
+    <li className='friend-index-item-container'>
+      <div>
+        <img src="https://res.cloudinary.com/dnmk6viwx/image/upload/v1638645241/profile-icon-png-898_3_ofszd1.png" alt="" />
+        <div style={{display:'inline-block'}}>{friend.friendEmail.substring(0, friend.friendEmail.lastIndexOf("@"))}</div>
+      </div>
+      <button className='delete-friend-button' onClick={() => deleteFriend(friend.id)}>X</button>
     </li> 
   )
 }
