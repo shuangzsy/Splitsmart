171e6cb02b9c502e5d2e624d408017606022958d
shuang zheng <shuangzheng16@gmail.com>
2022-01-05 22:57:28 -0800

    build friend-error-reducer

---
+++ frontend/reducers/friend_errors_reducer.js
@@ -0,0 +1,18 @@
+import {
+  RECEIVE_FRIEND_ERRORS,
+  REMOVE_FRIEND_ERRORS
+} from '../actions/friend_action';
+
+
+
+export default (state = [], action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_FRIEND_ERRORS:
+      return action.errors;
+    case REMOVE_FRIEND_ERRORS:
+      return [];
+    default:
+      return state;
+  }
+};
